d7f9971c2aa960db9a73de6d6d0095cbbaac45f4
Grld-Andy <andyansong82@gmail.com>
2026-07-18 06:14:33 +0000

    feat: api

--- src/api/requestService.ts
+++ src/api/requestService.ts
@@ -4,8 +4,9 @@ import { RequestTakeout } from "../types/request.types";
 
 export const requestService = {
   updateRequestStatus: async (id: string, status: "pending" | "accepted" | "en_route" | "arrived" | "completed" | "cancelled") => {
+    console.log("updating user status: ", status)
     const { data } = await api.patch<ApiResponse<{ request: RequestTakeout }>>(
-        `drivers/requests/${id}/status`,
+        `customer/requests/${id}/status`,
         { status }
     );
     return data;
